eb8e689beb3f69d4c0855e53a195659429f74bb6
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-09-09 15:18:41 +0200

    Migrate to PDO

--- src/main/consultationAttributions.php
+++ src/main/consultationAttributions.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 include("_debut.inc.php");
 include("_gestionBase.inc.php"); 
@@ -13,12 +13,14 @@ if (!$connexion)
    afficherErreurs();
    exit();
 }
+/*
 if (!selectBase($connexion))
 {
    ajouterErreur("La base de données festival est inexistante ou non accessible");
    afficherErreurs();
    exit();
 }
+*/
 
 // CONSULTER LES ATTRIBUTIONS DE TOUS LES ÉTABLISSEMENTS
 
