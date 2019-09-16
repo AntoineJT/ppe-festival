c6c59994451d30977ccbf461a39ba0000a444904
MattDuc <55089890+MattDuc@users.noreply.github.com>
2019-09-16 15:02:38 +0200

    Add bootstrap, some stuff

--- src/main/_debut.inc.php
+++ src/main/_debut.inc.php
@@ -1,6 +1,6 @@
 <?php
 echo '
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <!-- TITRE ET MENUS -->
 <html lang="fr">
@@ -8,28 +8,19 @@ echo '
         <title>Maison des Ligues</title>
         <meta http-equiv="Content-Language" content="fr">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
         <link href="css/cssGeneral.css" rel="stylesheet" type="text/css">
     </head>
-    <body class="basePage">
-        <!--  Tableau contenant le titre -->
-        <table width="100%" cellpadding="0" cellspacing="0">
-            <tr> 
-                <td class="titre">Maison des Ligues<br>
-                    <span id="texteNiveau2" class="texteNiveau2">
-                        H&eacute;bergement des groupes</span><br>&nbsp;
-                </td>
-            </tr>
-        </table>
+    <body class="p-3 mb-2 bg-white text-dark">
+        <div class="p-3 mb-2 bg-info text-white">
+          Festival Folklores du Monde <br>
+          H&eacute;bergement des groupes</span>
+        </div>
+        <nav class="nav nav_pills nav-fill">
+              <a class="nav-link nav-item" href="index.php">Accueil</a>
+              <a class="nav-link nav-item" href="listeEtablissements.php">Gestion établissements</a>
+              <a class="nav-link nav-item" href="consultationAttributions.php">Attributions chambres</a>
 
-        <!--  Tableau contenant les menus -->
-        <table width="80%" cellpadding="0" cellspacing="0" class="tabMenu" align="center">
-           <tr>
-              <td class="menu"><a href="index.php">Accueil</a></td>
-              <td class="menu"><a href="listeEtablissements.php">
-                      Gestion établissements</a></td>
-              <td class="menu"><a href="consultationAttributions.php">
-                      Attributions chambres</a></td>
-           </tr>
-        </table>
-        <br>
+        </nav>
+      </body>
 ';
