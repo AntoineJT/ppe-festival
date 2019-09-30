7906bb2578aff315345fb0abe8ba694f20511b4c
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-09-30 15:13:30 +0200

    Some aesthetic changes in SQL scripts

--- src/sql/create-utf8.sql
+++ src/sql/create-utf8.sql
@@ -1,43 +1,45 @@
-
 GRANT ALL ON festival . * TO 'festival'@'localhost' IDENTIFIED BY 'secret';
 
 DROP TABLE IF EXISTS `Attribution`;
 DROP TABLE IF EXISTS `Groupe`;
 DROP TABLE IF EXISTS `Etablissement`;
 
-create table Etablissement 
-(id char(8) not null,
-mdp CHAR(60) NOT NULL,
-nom varchar(45) not null,
-adresseRue varchar(45) not null, 
-codePostal char(5) not null, 
-ville varchar(35) not null,
-tel varchar(13) not null,
-adresseElectronique varchar(70),
-type tinyint not null,
-civiliteResponsable varchar(12) not null,
-nomResponsable varchar(25) not null,
-prenomResponsable varchar(25),
-nombreChambresOffertes integer,
-constraint pk_Etablissement primary key(id))
-ENGINE=INNODB;
+CREATE TABLE Etablissement 
+(
+    id char(8) NOT NULL,
+    mdp CHAR(60) NOT NULL,
+    nom varchar(45) NOT NULL,
+    adresseRue varchar(45) NOT NULL, 
+    codePostal char(5) NOT NULL, 
+    ville varchar(35) NOT NULL,
+    tel varchar(13) NOT NULL,
+    adresseElectronique varchar(70),
+    type tinyint NOT NULL,
+    civiliteResponsable varchar(12) NOT NULL,
+    nomResponsable varchar(25) NOT NULL,
+    prenomResponsable varchar(25),
+    nombreChambresOffertes integer,
+    CONSTRAINT pk_Etablissement PRIMARY KEY(id)
+) ENGINE=INNODB;
 
-create table Groupe
-(id char(4) not null, 
-nom varchar(40) not null, 
-identiteResponsable varchar(40) default null,
-adressePostale varchar(120) default null,
-nombrePersonnes integer not null, 
-nomPays varchar(40) not null, 
-hebergement char(1) not null,
-constraint pk_Groupe primary key(id))
-ENGINE=INNODB;
+CREATE TABLE Groupe
+(
+    id char(4) NOT NULL,
+    nom varchar(40) NOT NULL,
+    identiteResponsable varchar(40) default null,
+    adressePostale varchar(120) default null,
+    nombrePersonnes integer NOT NULL,
+    nomPays varchar(40) NOT NULL,
+    hebergement char(1) NOT NULL,
+    CONSTRAINT pk_Groupe PRIMARY KEY(id)
+) ENGINE=INNODB;
 
-create table Attribution
-(idEtab char(8) not null, 
-idGroupe char(4) not null, 
-nombreChambres integer not null,
-constraint pk_Attribution primary key(idEtab, idGroupe), 
-constraint fk1_Attribution foreign key(idEtab) references Etablissement(id),
-constraint fk2_Attribution foreign key(idGroupe) references Groupe(id)) 
-ENGINE=INNODB;
+CREATE TABLE Attribution
+(
+    idEtab char(8) NOT NULL,
+    idGroupe char(4) NOT NULL,
+    nombreChambres integer NOT NULL,
+    CONSTRAINT pk_Attribution PRIMARY KEY(idEtab, idGroupe),
+    CONSTRAINT fk1_Attribution FOREIGN KEY(idEtab) REFERENCES Etablissement(id),
+    CONSTRAINT fk2_Attribution FOREIGN KEY(idGroupe) REFERENCES Groupe(id)
+) ENGINE=INNODB;
--- src/sql/insert-utf8.sql
+++ src/sql/insert-utf8.sql
@@ -1,67 +1,67 @@
 
 -- Certains établissements sont fictifs
 -- mdp: Moka123
-insert into Etablissement values ('0350785N', '$2y$10$UMBQTbR4.6MMpJ.SPpzy8.Aq3aDVotLcYX.sHddJ0WPVBBpNb4lpS', 'Collège de Moka', '2 avenue Aristide Briand BP 6', '35401', 'Saint-Malo', '0299206990', null,1,'M.','Dupont','Alain',20);
+INSERT INTO Etablissement VALUES ('0350785N', '$2y$10$UMBQTbR4.6MMpJ.SPpzy8.Aq3aDVotLcYX.sHddJ0WPVBBpNb4lpS', 'Collège de Moka', '2 avenue Aristide Briand BP 6', '35401', 'Saint-Malo', '0299206990', null,1,'M.','Dupont','Alain',20);
 -- mdp: Mart321
-insert into Etablissement values ('0350123A', '$2y$10$FOYm.XYZoLmRrt1TY8ajheRt.zrA5R582nCP68Sh1.GTHHIG7KZ3G', 'Collège Lamartine', '3, avenue des corsaires', '35404', 'Paramé', '0299561459', null, 1,'Mme','Lefort','Anne',58);
+INSERT INTO Etablissement VALUES ('0350123A', '$2y$10$FOYm.XYZoLmRrt1TY8ajheRt.zrA5R582nCP68Sh1.GTHHIG7KZ3G', 'Collège Lamartine', '3, avenue des corsaires', '35404', 'Paramé', '0299561459', null, 1,'Mme','Lefort','Anne',58);
 -- mdp: LDV46
-insert into Etablissement values ('0351234W', '$2y$10$xRPDgij7kIf73UmAWfqWje35cbVbvOR4m6kYwc6C4VFLJrxvrhdci', 'Collège Léonard de Vinci', '2 rue Rabelais', '35418', 'Saint-Malo', '0299117474', null, 1,'M.','Durand','Pierre',60);
+INSERT INTO Etablissement VALUES ('0351234W', '$2y$10$xRPDgij7kIf73UmAWfqWje35cbVbvOR4m6kYwc6C4VFLJrxvrhdci', 'Collège Léonard de Vinci', '2 rue Rabelais', '35418', 'Saint-Malo', '0299117474', null, 1,'M.','Durand','Pierre',60);
 -- mdp: CRI37
-insert into Etablissement values ('11111111', '$2y$10$npOqUePDdKk9cugjv5eGDue6/dbJfVFDX9J6ARTyW5DtVsl7KUjf.', 'Centre de rencontres internationales', '37 avenue du R.P. Umbricht BP 108', '35407', 'Saint-Malo', '0299000000', null, 0, 'M.','Guenroc','Guy',200);
+INSERT INTO Etablissement VALUES ('11111111', '$2y$10$npOqUePDdKk9cugjv5eGDue6/dbJfVFDX9J6ARTyW5DtVsl7KUjf.', 'Centre de rencontres internationales', '37 avenue du R.P. Umbricht BP 108', '35407', 'Saint-Malo', '0299000000', null, 0, 'M.','Guenroc','Guy',200);
 
 -- Certains groupes sont incomplètement renseignés
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g001','Groupe folklorique du Bachkortostan',40,'Bachkirie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g002','Marina Prudencio Chavez',25,'Bolivie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g003','Nangola Bahia de Salvador',34,'Brésil','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g004','Bizone de Kawarma',38,'Bulgarie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g005','Groupe folklorique camerounais',22,'Cameroun','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g006','Syoung Yaru Mask Dance Group',29,'Corée du Sud','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g007','Pipe Band',19,'Ecosse','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g008','Aira da Pedra',5,'Espagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g009','The Jersey Caledonian Pipe Band',21,'Jersey','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g010','Groupe folklorique des Émirats',30,'Emirats arabes unis','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g011','Groupe folklorique mexicain',38,'Mexique','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g012','Groupe folklorique de Panama',22,'Panama','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g013','Groupe folklorique papou',13,'Papouasie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g014','Paraguay Ete',26,'Paraguay','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g015','La Tuque Bleue',8,'Québec','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g016','Ensemble Leissen de Oufa',40,'République de Bachkirie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g017','Groupe folklorique turc',40,'Turquie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g018','Groupe folklorique russe',43,'Russie','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g019','Ruhunu Ballet du village de Kosgoda',27,'Sri Lanka','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g020','L''Alen',34,'France - Provence','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g021','L''escolo Di Tourre',40,'France - Provence','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g022','Deloubes Kévin',1,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g023','Daonie See',5,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g024','Boxty',5,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g025','Soeurs Chauvel',2,'France - Bretagne','O');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g026','Cercle Gwik Alet',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g027','Bagad Quic En Groigne',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g028','Penn Treuz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g029','Savidan Launay',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g030','Cercle Boked Er Lann',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g031','Bagad Montfortais',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g032','Vent de Noroise',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g033','Cercle Strollad',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g034','Bagad An Hanternoz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g035','Cercle Ar Vro Melenig',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g036','Cercle An Abadenn Nevez',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g037','Kerc''h Keltiek Roazhon',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g038','Bagad Plougastel',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g039','Bagad Nozeganed Bro Porh-Loeiz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g040','Bagad Nozeganed Bro Porh-Loeiz',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g041','Jackie Molard Quartet',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g042','Deomp',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g043','Cercle Olivier de Clisson',0,'France - Bretagne','N');
-insert into Groupe (id, nom, nombrepersonnes, nompays, hebergement) values ('g044','Kan Tri',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g001','Groupe folklorique du Bachkortostan',40,'Bachkirie','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g002','Marina Prudencio Chavez',25,'Bolivie','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g003','Nangola Bahia de Salvador',34,'Brésil','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g004','Bizone de Kawarma',38,'Bulgarie','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g005','Groupe folklorique camerounais',22,'Cameroun','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g006','Syoung Yaru Mask Dance Group',29,'Corée du Sud','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g007','Pipe Band',19,'Ecosse','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g008','Aira da Pedra',5,'Espagne','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g009','The Jersey Caledonian Pipe Band',21,'Jersey','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g010','Groupe folklorique des Émirats',30,'Emirats arabes unis','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g011','Groupe folklorique mexicain',38,'Mexique','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g012','Groupe folklorique de Panama',22,'Panama','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g013','Groupe folklorique papou',13,'Papouasie','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g014','Paraguay Ete',26,'Paraguay','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g015','La Tuque Bleue',8,'Québec','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g016','Ensemble Leissen de Oufa',40,'République de Bachkirie','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g017','Groupe folklorique turc',40,'Turquie','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g018','Groupe folklorique russe',43,'Russie','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g019','Ruhunu Ballet du village de Kosgoda',27,'Sri Lanka','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g020','L''Alen',34,'France - Provence','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g021','L''escolo Di Tourre',40,'France - Provence','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g022','Deloubes Kévin',1,'France - Bretagne','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g023','Daonie See',5,'France - Bretagne','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g024','Boxty',5,'France - Bretagne','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g025','Soeurs Chauvel',2,'France - Bretagne','O');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g026','Cercle Gwik Alet',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g027','Bagad Quic En Groigne',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g028','Penn Treuz',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g029','Savidan Launay',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g030','Cercle Boked Er Lann',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g031','Bagad Montfortais',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g032','Vent de Noroise',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g033','Cercle Strollad',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g034','Bagad An Hanternoz',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g035','Cercle Ar Vro Melenig',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g036','Cercle An Abadenn Nevez',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g037','Kerc''h Keltiek Roazhon',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g038','Bagad Plougastel',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g039','Bagad Nozeganed Bro Porh-Loeiz',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g040','Bagad Nozeganed Bro Porh-Loeiz',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g041','Jackie Molard Quartet',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g042','Deomp',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g043','Cercle Olivier de Clisson',0,'France - Bretagne','N');
+INSERT INTO Groupe (id, nom, nombrepersonnes, nompays, hebergement) VALUES ('g044','Kan Tri',0,'France - Bretagne','N');
 
 -- Les attributions sont fictives
-insert into Attribution values ('0350785N', 'g001', 11);
-insert into Attribution values ('0350785N', 'g002', 9);
+INSERT INTO Attribution VALUES ('0350785N', 'g001', 11);
+INSERT INTO Attribution VALUES ('0350785N', 'g002', 9);
 
-insert into Attribution values ('0350123A', 'g004', 13);
-insert into Attribution values ('0350123A', 'g005', 8);
+INSERT INTO Attribution VALUES ('0350123A', 'g004', 13);
+INSERT INTO Attribution VALUES ('0350123A', 'g005', 8);
 
-insert into Attribution values ('0351234W', 'g001', 3);
-insert into Attribution values ('0351234W', 'g006', 10);
-insert into Attribution values ('0351234W', 'g007', 7);
+INSERT INTO Attribution VALUES ('0351234W', 'g001', 3);
+INSERT INTO Attribution VALUES ('0351234W', 'g006', 10);
+INSERT INTO Attribution VALUES ('0351234W', 'g007', 7);
