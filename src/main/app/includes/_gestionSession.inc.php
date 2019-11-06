6955139398054bf0c4b42396e318748172982503
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-06 09:58:17 +0100

    :bug: Fix session not working

--- src/main/app/includes/_gestionSession.inc.php
+++ src/main/app/includes/_gestionSession.inc.php
@@ -1,10 +1,15 @@
 <?php
-session_start();
 
-function gererSession($good_case){
-    if (is_null($_SESSION['compte'])){
+use \Illuminate\Support\Facades\Session;
+
+function gererSession($good_case)
+{
+    // TODO Rediriger vers page login quand sera fait
+    if (is_null(Session::get('compte'))) {
+        /*
         header('Location: login.php');
-    } else {
-        $good_case();
+        return;
+        */
     }
+    $good_case();
 }
