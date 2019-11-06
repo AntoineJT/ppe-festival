eaa43bb6db343946fc7e42cf179cc8d65d7d7772
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-06 11:36:55 +0100

    :bug: Add title to login page

--- src/main/resources/views/pages/login.blade.php
+++ src/main/resources/views/pages/login.blade.php
@@ -57,15 +57,16 @@
 <head>
     <link href="css/connect.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <title>M2L - Connexion</title>
 </head>
 <body class="text-center">
-<form method="POST" class="form-signin">
-    <img class="mb-4" src="images/mdl.png" class="rounded float-right" alt="Responive image">
-    <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
-    <label for="etablissement" class="sr-only">ID</label>
-    <input type="text" name="etablissement" class="form-control" placeholder="ID" required autofocus>
-    <label for="mdp" class="sr-only">Mot de Passe</label>
-    <input type="password" name="mdp" class="form-control" placeholder="Mot de Passe" required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Se Connecter</button>
-</form>
+    <form method="POST" class="form-signin">
+        <img class="mb-4" src="images/mdl.png" class="rounded float-right" alt="Responive image">
+        <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+        <label for="etablissement" class="sr-only">ID</label>
+        <input type="text" name="etablissement" class="form-control" placeholder="ID" required autofocus>
+        <label for="mdp" class="sr-only">Mot de Passe</label>
+        <input type="password" name="mdp" class="form-control" placeholder="Mot de Passe" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Se Connecter</button>
+    </form>
 </body>
