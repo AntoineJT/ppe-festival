4f0590c4b5c9ae4a077a4c2e8ba388dfec09e31b
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-13 09:37:26 +0100

    :fire: Remove useless things in login page

--- src/main/resources/views/pages/login.blade.php
+++ src/main/resources/views/pages/login.blade.php
@@ -15,42 +15,26 @@
         if ($etablissement == NULL || $mdp == NULL) {
             die("Les champs obligatoires n'ont pas été remplis correctement");
         }
-    /*
-    if (isset($_POST['etablissement'])){
-        $etablissement = isset($_POST['etablissement']) ? $_POST['etablissement'] : null;
-        $mdp = isset($_POST['mdp']) ? $_POST['mdp'] : null;
 
-        if (is_null($etablissement) || is_null($mdp)){
-            die("Les champs obligatoires n'ont pas été remplis correctement");
-        }
-        */
         $connexion = connect();
         // TODO Move that into gestionBase
         $request = $connexion->prepare('SELECT mdp FROM Administrateur WHERE nomAdmin = ?');
-        /*
-        // Temporary thing to generate hash easily
-        $toHash = $request->execute([$etablissement]);
-        $hash = password_hash($toHash, PASSWORD_ARGON2I);
-        echo $hash;
-        */
+
         if (!$request->execute([$etablissement])){
             die('Une erreur avec la base de données est survenue!');
         }
+
         $hash = $request->fetch()[0];
         if (!password_verify($mdp, $hash)){
             die('Le couple identifiant/mot de passe est invalide!');
         }
-        // $_SESSION['compte'] = $etablissement;
+
         Request::session()->put('compte', $etablissement);
-        // header('Location: index.php');
         redirect()->route('accueil');
     }
 
-    // if (isset($_GET['disconnect'])){
     if (Request::has('disconnect')) {
-        // $_SESSION['compte'] = null;
         Request::session()->remove('compte');
-        // header('Location: login.php');
         redirect()->route('login');
     }
 @endphp
