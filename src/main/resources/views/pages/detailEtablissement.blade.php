7c4aca77a5d6a85bfc85ea6c8484811502aabf24
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-20 11:15:28 +0100

    :art: Make detailEtablissement code less ugly

--- src/main/resources/views/pages/detailEtablissement.blade.php
+++ src/main/resources/views/pages/detailEtablissement.blade.php
@@ -1,13 +1,5 @@
 @php
-
-// include '_gestionSession.inc.php';
-
-// gererSession(function(){
-//    include("_debut.inc.php");
     include(app_path() . '/includes/_gestionBase.inc.php');
-//    include("_controlesEtGestionErreurs.inc.php");
-
-// CONNEXION AU SERVEUR MYSQL PUIS SÉLECTION DE LA BASE DE DONNÉES festival
 
     $connexion=connect();
     /*
@@ -25,74 +17,76 @@
 
 // OBTENIR LE DÉTAIL DE L'ÉTABLISSEMENT SÉLECTIONNÉ
 
-    $lgEtab=obtenirDetailEtablissement($connexion, $id);
+    $lgEtab = obtenirDetailEtablissement($connexion, $id);
 
-    $nom=$lgEtab['nom'];
-    $adresseRue=$lgEtab['adresseRue'];
-    $codePostal=$lgEtab['codePostal'];
-    $ville=$lgEtab['ville'];
-    $tel=$lgEtab['tel'];
-    $adresseElectronique=$lgEtab['adresseElectronique'];
-    $type=$lgEtab['type'];
-    $civiliteResponsable=$lgEtab['civiliteResponsable'];
-    $nomResponsable=$lgEtab['nomResponsable'];
-    $prenomResponsable=$lgEtab['prenomResponsable'];
-    $nombreChambresOffertes=$lgEtab['nombreChambresOffertes'];
+    $nom = $lgEtab['nom'];
+    $adresseRue = $lgEtab['adresseRue'];
+    $codePostal = $lgEtab['codePostal'];
+    $ville = $lgEtab['ville'];
+    $tel = $lgEtab['tel'];
+    $adresseElectronique = $lgEtab['adresseElectronique'];
+    $type = $lgEtab['type'];
+    $civiliteResponsable = $lgEtab['civiliteResponsable'];
+    $nomResponsable = $lgEtab['nomResponsable'];
+    $prenomResponsable = $lgEtab['prenomResponsable'];
+    $nombreChambresOffertes = $lgEtab['nombreChambresOffertes'];
 
 @endphp
 
-    <table class='mt-4 table table-bordered table-dark'>
-       <thead class='thead-light'>
+<table class='mt-4 table table-bordered table-dark'>
+    <thead class='thead-light'>
         <tr>
-          <th colspan='2'>{{ $nom }}</th>
+            <th colspan='2'>{{ $nom }}</th>
         </tr>
-       </thead>
-       <tbody>
-       <tr>
-        <td>Id</td>
-        <td>{{ $id }}</td>
-      </tr>
-      <tr>
-        <td>Adresse</td>
-        <td>{{ $adresseRue }}</td>
-      </tr>
-      <tr>
-        <td>Code postal</td>
-        <td>{{ $codePostal }}</td>
-      </tr>
-      <tr>
-        <td>Ville</td>
-        <td>{{ $ville }}</td>
-      </tr>
-      <tr>
-        <td>Téléphone</td>
-        <td>{{ $tel }}</td>
-      </tr>
-      <tr>
-      <td>E-mail</td>
-      <td>{{ $adresseElectronique }}</td>
-   </tr>
-   <tr>
-      <td>Type</td>
-    @php
-       if ($type==1)
-    {
-        echo "<td> Etablissement scolaire </td>";
-    }
-    else
-    {
-        echo "<td> Autre établissement </td>";
-    }
-    @endphp
-   </tr>
-   <tr>
-      <td>Responsable</td>
-      <td>{{ $civiliteResponsable }}&nbsp; {{ $nomResponsable }}&nbsp; {{ $prenomResponsable }}
-      </td>
-   </tr>
-   <tr>
-      <td>Offre</td>
-      <td>{{ $nombreChambresOffertes }}&nbsp;chambre(s)</td>
-   </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Id</td>
+            <td>{{ $id }}</td>
+        </tr>
+        <tr>
+            <td>Adresse</td>
+            <td>{{ $adresseRue }}</td>
+        </tr>
+        <tr>
+            <td>Code postal</td>
+            <td>{{ $codePostal }}</td>
+        </tr>
+        <tr>
+            <td>Ville</td>
+            <td>{{ $ville }}</td>
+        </tr>
+        <tr>
+            <td>Téléphone</td>
+            <td>{{ $tel }}</td>
+        </tr>
+        <tr>
+            <td>E-mail</td>
+            <td>{{ $adresseElectronique }}</td>
+        </tr>
+        <tr>
+            <td>Type</td>
+            <td>
+            @php
+                if ($type==1)
+                {
+                    echo 'Etablissement scolaire';
+                }
+                else
+                {
+                    echo 'Autre établissement';
+                }
+            @endphp
+            </td>
+        </tr>
+        <tr>
+            <td>Responsable</td>
+            <td>{{ $civiliteResponsable }}&nbsp; {{ $nomResponsable }}&nbsp; {{ $prenomResponsable }}</td>
+        </tr>
+        <tr>
+            <td>Offre</td>
+            <td>{{ $nombreChambresOffertes }}&nbsp;chambre(s)</td>
+        </tr>
+    </tbody>
 </table>
 <a href='listeEtablissements.php' class='btn btn-outline-info active' >Retour</a>
