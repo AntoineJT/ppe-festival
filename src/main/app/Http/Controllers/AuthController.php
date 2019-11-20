63d0652e13a069ad61ec459d1b0b919e3769fede
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-11-20 10:31:19 +0100

    :fire: Remove unused import

--- src/main/app/Http/Controllers/AuthController.php
+++ src/main/app/Http/Controllers/AuthController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Validator;
